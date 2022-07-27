afaee15bb355ee6bb94b1bd20b970f9363609490
Samuel Motta <samuelsamuca953@gmail.com>
2022-07-27 22:51:41 +0000

    CallPanel component receiving data

--- src/components/CallPanel.tsx
+++ src/components/CallPanel.tsx
@@ -1,8 +1,13 @@
+import { Data } from "../pages";
 
-export default function CallPanel() {
+
+export default function CallPanel({ actualCall, lastCalls }: Data) {
     return (
-        <div className="bg-stone-400 text-6xl">
-            <h1>Inside div</h1>
-        </div>
+        <>
+            <main className="bg-stone-400">
+                <h1 className="text-4xl">{actualCall.name || actualCall.cpf}</h1>
+                <p>{actualCall.type}</p>
+            </main>
+        </>
     )
-} 
+}
