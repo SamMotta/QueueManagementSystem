e00deeb3b49e0c4ae401fb1f63f80e27227889e0
Samuel Motta <samuelsamuca953@gmail.com>
2022-07-27 22:50:03 +0000

    starting dynamic routes for counters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import CallPanel from '../components/CallPanel'
 
-const Home: NextPage = () => {
+export type QueueType = {
+  // uuid: string;
+  name?: string;
+  cpf: string;
+  type: string;
+}
+
+export type Data = {
+  actualCall: QueueType;
+  lastCalls: Array<QueueType>;
+}
+
+const Home: NextPage<Data> = ({ actualCall, lastCalls }: Data) => {
+  console.log(actualCall)
+  console.table(lastCalls)
   return (
     <div>
       <Head>
@@ -11,9 +25,22 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Hello World!</h1>
-      <CallPanel />
+      <CallPanel actualCall={actualCall} lastCalls={lastCalls} />
     </div>
   )
 }
 
 export default Home
+
+// Socket.io? PS: React-query with refetchInterval: 1000;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch('https://3000-sammotta-queuemanagemen-3ryqhh6dkh2.ws-us54.gitpod.io/api/queue')
+  const { actualCall, lastCalls }: Data = await res.json()
+
+  return {
+    props: {
+      actualCall,
+      lastCalls
+    },
+  }
+}
